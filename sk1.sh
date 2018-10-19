5343a899758c11de67928d629cc670d13a455718
nataliaw6 <44300292+nataliaw6@users.noreply.github.com>
2018-10-19 18:57:04 +0200

    Update sk1.sh

--- sk1.sh
+++ sk1.sh
@@ -6,5 +6,5 @@ echo $dzien
 if [ $godz -ge 13 ] && [ $godz -le 15 ] && [ $dzien -ge 2 ] && [ $dzien -le 2 ]
 then
 echo 'Jestes na zajęciach'
-else echo 'Nie ma Cie na zajęciach'
+else echo 'Nie ma Pana na zajęciach'
 fi
